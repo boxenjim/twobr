4d0e89ecf7dec6a81ec2f7d130a8357a85890541
Jim Schultz <boxenjim@gmail.com>
2015-02-19 23:24:03 -0800

    added call to search endpoint, if no posts are found in user feed, automatically searches all of twitter, can now remove posts from job feed, essentially acts as a favorites feed as you clear them out

--- Twobr/JobsViewController.swift
+++ Twobr/JobsViewController.swift
@@ -15,8 +15,13 @@ let defaultAvatarURL = NSURL(string: "https://abs.twimg.com/sticky/default_profi
 class JobsViewController: UITableViewController, TwitterAPIRequestDelegate {
     
     var jobDetailsVC: JobDetailsViewController? = nil
-    var sinceID: String? = nil
-    var matchedTweets: [ParsedTweet] = []
+    
+    var searchRequest: TwitterAPIRequest? = nil
+    var feedRequest: TwitterAPIRequest? = nil
+    
+    var feedSinceID: String? = nil
+    var searchSinceID: String? = nil
+    var jobsArray: [ParsedTweet] = []
     
     @IBAction func handleTweetButtonTapped(sender: AnyObject) {
         if SLComposeViewController.isAvailableForServiceType(SLServiceTypeTwitter) {
@@ -30,31 +35,134 @@ class JobsViewController: UITableViewController, TwitterAPIRequestDelegate {
     
     @IBAction func handleRefresh(sender : AnyObject?) {
         self.reloadTweets()
-        refreshControl!.endRefreshing()
     }
     
     func reloadTweets() {
-        let accountStore = ACAccountStore()
-        let twitterAccountType = accountStore.accountTypeWithAccountTypeIdentifier(ACAccountTypeIdentifierTwitter)
-        accountStore.requestAccessToAccountsWithType(twitterAccountType, options: nil, completion: {
-            (granted: Bool, error: NSError!) -> Void in
-            if !granted {
-                println("account access not granted")
-            } else {
-                let twitterAccounts = accountStore.accountsWithAccountType(twitterAccountType)
-                if twitterAccounts.count == 0 {
-                    println("no twitter accounts configured")
-                } else {
-                    var twitterParams = ["count":"100"]
-                    if self.sinceID != nil {
-                        twitterParams["since_id"] = self.sinceID
+        
+        if feedRequest == nil && searchRequest == nil {
+            feedRequest = TwitterAPIRequest()
+            searchRequest = TwitterAPIRequest()
+            
+            // get home timeline
+            var feedParams = ["count":"20"]
+            if self.feedSinceID != nil {
+                feedParams["since_id"] = self.feedSinceID
+            }
+            
+            let feedAPIURL = NSURL(string: "https://api.twitter.com/1.1/statuses/home_timeline.json")
+            feedRequest!.sendTwitterRequest(feedAPIURL, params: feedParams, delegate: self)
+        }
+    }
+    
+    func searchForJobs() {
+        // get search results
+        var searchParams = ["count":"20", "q":"job, hire, hiring"]
+        if self.searchSinceID != nil {
+            searchParams["since_id"] = self.searchSinceID
+        }
+        
+        let searchAPIURL = NSURL(string: "https://api.twitter.com/1.1/search/tweets.json")
+        searchRequest!.sendTwitterRequest(searchAPIURL, params: searchParams, delegate: self)
+    }
+    
+    func parseFeedRequestResponse(jsonArray: [[String:AnyObject]]) {
+        var tempArray: [ParsedTweet] = []
+        for tweetDict in jsonArray {
+            //println("tweet: \(tweetDict)")
+            //let keywords = ["open", "available", "fill", "work", "job", "hire", "hiring", "career", "look", "need", "position", "search", "find", "help", "grow", "join", "apply", "application", "full-time", "part-time", "full time", "part time", "contractor", "freelance"]
+            let keywords = ["job", "career", "hire", "hiring", "find", "looking", "part-time", "full-time", "freelance", "part time", "full time", "position", "twobr", "post", "interview"]
+            
+            var score = 0
+            let scoreToBeat = 1
+            if let text = tweetDict["text"] as? String {
+                for keyword in keywords {
+                    if text.lowercaseString.rangeOfString(keyword) != nil {
+                        score += 1
                     }
-                    
-                    let twitterAPIURL = NSURL(string: "https://api.twitter.com/1.1/statuses/home_timeline.json")
-                    let request = TwitterAPIRequest()
-                    request.sendTwitterRequest(twitterAPIURL, params: twitterParams, delegate: self)
+                    if score > scoreToBeat { break }
                 }
             }
+            
+            if let retweetedStatus = tweetDict["retweeted_status"] as? [String:AnyObject] {
+                if let entities = retweetedStatus["entities"] as? [String:AnyObject] {
+                    if let hashtags = entities["hashtags"] as? [[String:AnyObject]] {
+                        for hashtag in hashtags {
+                            if let text = hashtag["text"] as? String {
+                                for keyword in keywords {
+                                    if text.lowercaseString.rangeOfString(keyword) != nil {
+                                        score += 1
+                                    }
+                                    if score > scoreToBeat { break }
+                                }
+                            }
+                        }
+                    }
+                    if let urls = entities["urls"] as? [[String:AnyObject]] {
+                        for urlDict in urls {
+                            if let url = urlDict["display_url"] as? String {
+                                for keyword in keywords {
+                                    if url.lowercaseString.rangeOfString(keyword) != nil {
+                                        score += 1
+                                    }
+                                    if score > scoreToBeat { break }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            
+            if score > scoreToBeat {
+            //if score >= 0 {
+                let parsedTweet = ParsedTweet()
+                parsedTweet.tweetIdString = tweetDict["id_str"] as? String
+                parsedTweet.tweetText = tweetDict["text"] as? String
+                parsedTweet.createdAt = tweetDict["created_at"] as? String
+                let userDict = tweetDict["user"] as NSDictionary
+                parsedTweet.userName = userDict["name"] as? String
+                parsedTweet.userAvatarURL = NSURL(string: userDict["profile_image_url"] as String!)
+                tempArray.append(parsedTweet)
+            }
+        }
+        if tempArray.count > 0 {
+            self.feedSinceID = jsonArray[0]["id_str"] as? String
+            self.jobsArray[0..<0] = tempArray[0..<tempArray.count]
+        } else {
+            self.searchForJobs()
+        }
+        
+        self.feedRequest = nil
+        
+        dispatch_async(dispatch_get_main_queue(), {
+            self.tableView.reloadSections(NSIndexSet(indexesInRange: NSMakeRange(0, 1)), withRowAnimation: UITableViewRowAnimation.Automatic)
+            if tempArray.count > 0 {
+                self.refreshControl!.endRefreshing()
+            }
+        })
+    }
+    
+    func parseSearchRequestResponse(jsonArray: [[String:AnyObject]]) {
+        var tempArray: [ParsedTweet] = []
+        for tweetDict in jsonArray {
+            let parsedTweet = ParsedTweet()
+            parsedTweet.tweetIdString = tweetDict["id_str"] as? String
+            parsedTweet.tweetText = tweetDict["text"] as? String
+            parsedTweet.createdAt = tweetDict["created_at"] as? String
+            let userDict = tweetDict["user"] as NSDictionary
+            parsedTweet.userName = userDict["name"] as? String
+            parsedTweet.userAvatarURL = NSURL(string: userDict["profile_image_url"] as String!)
+            tempArray.append(parsedTweet)
+        }
+        if tempArray.count > 0 {
+            self.searchSinceID = jsonArray[0]["id_str"] as? String
+            self.jobsArray[0..<0] = tempArray[0..<tempArray.count]
+        }
+        
+        self.searchRequest = nil;
+        
+        dispatch_async(dispatch_get_main_queue(), {
+            self.tableView.reloadSections(NSIndexSet(indexesInRange: NSMakeRange(0, 1)), withRowAnimation: UITableViewRowAnimation.Automatic)
+            self.refreshControl!.endRefreshing()
         })
     }
     
@@ -69,74 +177,19 @@ class JobsViewController: UITableViewController, TwitterAPIRequestDelegate {
             
             dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), {
                 if let jsonArray = jsonObject as? [[String:AnyObject]] {
-                    var tempArray: [ParsedTweet] = []
-                    for tweetDict in jsonArray {
-                        //println("tweet: \(tweetDict)")
-                        //let keywords = ["open", "available", "fill", "work", "job", "hire", "hiring", "career", "look", "need", "position", "search", "find", "help", "grow", "join", "apply", "application", "full-time", "part-time", "full time", "part time", "contractor", "freelance"]
-                        let keywords = ["job", "career", "hire", "hiring", "find", "looking", "part-time", "full-time", "freelance", "part time", "full time", "position", "twobr", "post", "interview"]
-                        
-                        var score = 0
-                        let scoreToBeat = 1
-                        if let text = tweetDict["text"] as? String {
-                            for keyword in keywords {
-                                if text.lowercaseString.rangeOfString(keyword) != nil {
-                                    score += 1
-                                }
-                                if score > scoreToBeat { break }
-                            }
-                        }
-                        
-                        if let retweetedStatus = tweetDict["retweeted_status"] as? [String:AnyObject] {
-                            if let entities = retweetedStatus["entities"] as? [String:AnyObject] {
-                                if let hashtags = entities["hashtags"] as? [[String:AnyObject]] {
-                                    for hashtag in hashtags {
-                                        if let text = hashtag["text"] as? String {
-                                            for keyword in keywords {
-                                                if text.lowercaseString.rangeOfString(keyword) != nil {
-                                                    score += 1
-                                                }
-                                                if score > scoreToBeat { break }
-                                            }
-                                        }
-                                    }
-                                }
-                                if let urls = entities["urls"] as? [[String:AnyObject]] {
-                                    for urlDict in urls {
-                                        if let url = urlDict["display_url"] as? String {
-                                            for keyword in keywords {
-                                                if url.lowercaseString.rangeOfString(keyword) != nil {
-                                                    score += 1
-                                                }
-                                                if score > scoreToBeat { break }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                        
-                        //if score > scoreToBeat {
-                        if score >= 0 {
-                            let parsedTweet = ParsedTweet()
-                            parsedTweet.tweetIdString = tweetDict["id_str"] as? String
-                            parsedTweet.tweetText = tweetDict["text"] as? String
-                            parsedTweet.createdAt = tweetDict["created_at"] as? String
-                            let userDict = tweetDict["user"] as NSDictionary
-                            parsedTweet.userName = userDict["name"] as? String
-                            parsedTweet.userAvatarURL = NSURL(string: userDict["profile_image_url"] as String!)
-                            tempArray.append(parsedTweet)
+                    if fromRequest == self.feedRequest {
+                        self.parseFeedRequestResponse(jsonArray)
+                    } else if fromRequest == self.searchRequest {
+                        self.parseSearchRequestResponse(jsonArray)
+                    }
+                } else if let jsonDict = jsonObject as? [String:AnyObject] {
+                    if let jsonArray = jsonDict["statuses"] as? [[String:AnyObject]] {
+                        if fromRequest == self.feedRequest {
+                            self.parseFeedRequestResponse(jsonArray)
+                        } else if fromRequest == self.searchRequest {
+                            self.parseSearchRequestResponse(jsonArray)
                         }
                     }
-                    if tempArray.count > 0 {
-                        self.sinceID = jsonArray[0]["id_str"] as? String
-                        self.matchedTweets[0..<0] = tempArray[0..<tempArray.count]
-                    }
-                    dispatch_async(dispatch_get_main_queue(), {
-                        self.tableView.reloadData()
-                        if self.matchedTweets.count == 0 {
-                            // no jobs need to tell the user
-                        }
-                    })
                 } else {
                     println("json: \(jsonObject)")
                 }
@@ -157,6 +210,8 @@ class JobsViewController: UITableViewController, TwitterAPIRequestDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.navigationItem.leftBarButtonItem = editButtonItem()
+        
         if let split = self.splitViewController {
             let controllers = split.viewControllers
             self.jobDetailsVC = controllers[controllers.count-1].topViewController as? JobDetailsViewController
@@ -176,7 +231,7 @@ class JobsViewController: UITableViewController, TwitterAPIRequestDelegate {
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "showJobDetailsSegue" {
             if let indexPath = self.tableView.indexPathForSelectedRow() {
-                let parsedTweet = matchedTweets[indexPath.row] as ParsedTweet
+                let parsedTweet = jobsArray[indexPath.row] as ParsedTweet
                 let controller = (segue.destinationViewController as UINavigationController).topViewController as JobDetailsViewController
                 controller.tweetIdString = parsedTweet.tweetIdString
                 controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
@@ -187,12 +242,12 @@ class JobsViewController: UITableViewController, TwitterAPIRequestDelegate {
 
     // UITableViewDelegate UITableViewDataSource
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return matchedTweets.count
+        return jobsArray.count
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("ParsedTweetCell") as ParsedTweetCell
-        let parsedTweet = matchedTweets[indexPath.row]
+        let parsedTweet = jobsArray[indexPath.row]
         cell.userNameLabel.text = parsedTweet.userName
         cell.tweetTextLabel.text = parsedTweet.tweetText
         
@@ -220,6 +275,20 @@ class JobsViewController: UITableViewController, TwitterAPIRequestDelegate {
             })
         }
         return cell
+    }
+    
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            jobsArray.removeAtIndex(indexPath.row)
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+        }
     }
 }
 
